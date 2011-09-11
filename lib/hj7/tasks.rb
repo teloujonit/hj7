9db0818d34e7ae2416f05f88cf4ed3e78d48fccd
Louis T <negonicrac@gmail.com>
2011-09-11 13:50:24 -0400

    use fog to deploy site

--- lib/hj7/tasks.rb
+++ lib/hj7/tasks.rb
@@ -49,3 +49,60 @@ desc "watch the project for changes"
 task :watch do
   system "bundle exec jekyll"
 end
+
+desc 'generate and deploy website via fog'
+task :deploy => :default do
+  puts '>>> DEPLOYING SITE <<<'
+
+  configs = YAML::load_file('_fog.yml')
+
+  src    = File.expand_path('_site')
+  bucket = configs.delete(:bucket) || configs.delete(:bucket_name)
+  path   = nil
+
+  puts 'Connecting'
+  connection = ::Fog::Storage.new(configs)
+
+  # Get bucket
+  puts 'Getting bucket'
+  begin
+    directory = connection.directories.get(bucket)
+  rescue ::Excon::Errors::NotFound
+    should_create_bucket = true
+  end
+  should_create_bucket = true if directory.nil?
+
+  # Create bucket if necessary
+  if should_create_bucket
+    directory = connection.directories.create(key: bucket)
+  end
+
+  # Get list of remote files
+  files = directory.files
+  truncated = files.respond_to?(:is_truncated) && files.is_truncated
+  while truncated
+    set = directory.files.all(marker: files.last.key)
+    truncated = set.is_truncated
+    files = files + set
+  end
+
+  # Delete all the files in the bucket
+  puts 'Removing remote files'
+  files.all.each do |file|
+    file.destroy
+  end
+
+  # Upload all the files in the output folder to the clouds
+  puts 'Uploading local files'
+  FileUtils.cd(src) do
+    files = Dir['**/*'].select { |f| File.file?(f) }
+    files.each do |file_path|
+      directory.files.create(
+        key: "#{path}#{file_path}",
+        body: File.open(file_path),
+        public: true)
+    end
+  end
+
+  puts 'Done!'
+end
